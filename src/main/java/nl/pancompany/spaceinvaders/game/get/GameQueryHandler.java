a745efe00f488d36d9d07378de9ccdcf6f11c8d1
Auke Haanstra <ahaanstra@pancompany.com>
2025-11-06 01:03:41 +0100

    Smaller unimplemented part.

--- src/main/java/nl/pancompany/spaceinvaders/game/get/GameQueryHandler.java
+++ src/main/java/nl/pancompany/spaceinvaders/game/get/GameQueryHandler.java
@@ -1,10 +1,34 @@
 package nl.pancompany.spaceinvaders.game.get;
 
+import nl.pancompany.eventstore.annotation.EventHandler;
+import nl.pancompany.eventstore.annotation.ResetHandler;
+import nl.pancompany.spaceinvaders.events.GameCreated;
+import nl.pancompany.spaceinvaders.events.GameResumed;
+import nl.pancompany.spaceinvaders.events.GameStopped;
+
 import java.util.Optional;
 
+// Actually query handler, projector and repository in one.
 public class GameQueryHandler {
 
-    public Optional<GameReadModel> get(GetGame getGame) {
-        return Optional.empty();
+    private GameReadModel gameReadModel; // repo
+
+    @ResetHandler
+    void reset() {
+        gameReadModel = new GameReadModel(false, true, Optional.empty());
+    }
+
+    @EventHandler // projector
+    private void update(GameCreated gameCreated) {
+        gameReadModel = new GameReadModel(true, false, Optional.empty());
+    }
+
+    @EventHandler
+    private void update(GameResumed gameResumed) {
+        gameReadModel = new GameReadModel(true, false, Optional.empty());
+    }
+
+    public Optional<GameReadModel> get(GetGame getGame) { // query handler
+        return Optional.ofNullable(gameReadModel);
     }
 }
